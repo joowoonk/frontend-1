9421064dd304b5fe38543453ac907f6d72fa6fe5
Justin Lohner <lohner4@gmail.com>
2020-04-28 11:20:08 -0600

    Deleted unnecessary comments

--- frontend/src/pages/Registration/SignUpPage.js
+++ frontend/src/pages/Registration/SignUpPage.js
@@ -3,46 +3,30 @@ import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 
 import { useHistory } from "react-router-dom";
-// import axios from 'axios';
 import * as yup from "yup";
 
 import SignUpForm from "./SignUpForm.js";
 import { axiosWithAuth } from "../../utils/axiosWithAuth.js";
 
-// const url = '';
-
 const initFormValues = {
-  //   firstName: "",
-  //   lastName: "",
   username: "",
-  //   email: "",
   password: "",
   confirmPassword: "",
   terms: false,
 };
 
 const initFormErrors = {
-  //   firstName: "",
-  //   lastName: "",
   username: "",
-  //   email: "",
   password: "",
   confirmPassword: "",
   terms: "",
 };
 
 const formSchema = yup.object().shape({
-  //   firstName: yup.string().required("First name is required"),
-  // .min(2)?
-  //   lastName: yup.string().required("Last name is required"),
   username: yup
     .string()
     .required("Username is required")
     .min(3, "Username must have a minimum of 3 characters"),
-  //   email: yup
-  //     .string()
-  //     .required("Email is required")
-  //     .email("Please enter a valid email address"),
   password: yup.string().required("Password is required"),
   confirmPassword: yup
     .string()
@@ -62,16 +46,6 @@ function SignUpPage() {
   const [formErrors, setFormErrors] = useState(initFormErrors);
 
   const { push } = useHistory();
-  //   console.log(formValues);
-  // const postUser = user => {
-  // 	axios.post(url)
-  // 		.then(res => {
-  // 			setUsers([...users, res.data]);
-  // 		})
-  // 		.catch(err => {
-  // 			console.log('error from trying to post user');
-  // 		})
-  // }
 
   const onInputChange = (evt) => {
     const name = evt.target.name;
